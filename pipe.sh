07a204c7f83f6777c163568c92641acedc93a6c6
Paris Kasidiaris <paris@sourcelair.com>
2019-06-25 11:16:29 +0300

    Ignore standard input and format output

--- pipe.sh
+++ pipe.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-http POST ${CI_SYSTEM_HOST}/api/deployment/ "Authorization: Token ${CI_SYSTEM_TOKEN}" repo=${BITBUCKET_REPO_FULL_NAME} ref=${BITBUCKET_BRANCH} commit=${BITBUCKET_COMMIT}
+http POST ${CI_SYSTEM_HOST}/api/deployment/ \
+     "Authorization: Token ${CI_SYSTEM_TOKEN}" \
+     repo=${BITBUCKET_REPO_FULL_NAME} \
+     ref=${BITBUCKET_BRANCH} \
+     commit=${BITBUCKET_COMMIT} \
+     --pretty=format \
+     --ignore-stdin
